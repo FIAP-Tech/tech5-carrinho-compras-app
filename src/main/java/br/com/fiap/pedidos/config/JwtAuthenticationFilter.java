b62ccd1314c27b4ac1c00c2cc9844988f2ef0114
devops.jeffersantos <devops.jeffersantos@gmail.com>
2024-07-15 23:58:19 -0300

    class JwtAuthenticationFilter

--- src/main/java/br/com/fiap/pedidos/config/JwtAuthenticationFilter.java
+++ src/main/java/br/com/fiap/pedidos/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package br.com.fiap.pedidos.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
